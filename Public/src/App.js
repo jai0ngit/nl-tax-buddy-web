3210314615ace4001d92f152d90ef1d796dff329
Jay <47306196+jai0ngit@users.noreply.github.com>
2025-08-28 15:46:35 +0200

    Delete Public/src directory

--- Public/src/App.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import LandingPage from './LandingPage';
-import MainApp from './MainApp';
-export default function App(){
-  const [started, setStarted] = useState(false);
-  return started ? <MainApp /> : <LandingPage onStart={() => setStarted(true)} />;
-}
